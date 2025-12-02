44c76d5a5b40a3899fd8063aa9ead2115c5cabfc
arosaspa <arosaspa@engineer.com>
2025-12-01 21:59:51 -0500

    Code

---
+++ Project.py
@@ -0,0 +1,19 @@
+# expense tracker project
+
+message = """\nWelcome the Expense Tracker Project\n 
+        ======== MENU ======== 
+        
+        1. Add Expense
+        2. View Expenses
+        3. View Total Spending
+        4. Exit
+        ======================
+                
+        Please select an option (1-4):
+    """
+
+print(message)
+
+option = int(input(">> "))
+
+print(f"You selected option {option}.")
